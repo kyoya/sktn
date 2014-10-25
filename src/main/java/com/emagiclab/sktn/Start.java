f19e2ed99fccffcac88854eae45ddf1ea77686e7
kyoya <anaheim78@gmail.com>
2014-10-26 02:38:13 +0900

    First

---
+++ src/main/java/com/emagiclab/sktn/Start.java
@@ -0,0 +1,10 @@
+package com.emagiclab.sktn;
+
+/**
+ * Created by kyoya on 2014/10/26.
+ */
+public class Start {
+    public static void main(String[] args){
+        System.out.println("xxxxxxxx hello");
+    }
+}
